f6ac737f69f0f8f33e76df8dbee87c250def41ed
Mike Figueroa <Mike Figueroa@FigsDesktop>
2018-02-07 02:45:15 -0800

    A1 Assignment Complete
    -Synchronize started

---
+++ A1_Figueroa/A1_1/a1_1.cpp
@@ -0,0 +1,25 @@
+/*-------------------------------------------------------------------
+/	Author:		Michael Figueroa				 
+/	Date:		Feb 6, 2018							 
+/	Class:		CSC 242							 
+/	Professor:	Dr. Ahmad R. Hadaegh
+/	Assignment: A1_1
+---------------------------------------------------------------------
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	cout<<"Name:       		\tMichael Figueroa" <<endl;
+	cout<<"Email:      		\tMichael.Figueroa73@gmail.com" <<endl;
+	cout<<"Student ID:		\t040910483" <<endl;
+	cout<<"Instructor Name: \t\tDr. Ahmad R. Hadaegh"<<endl;
+	cout<<"Course: 		    \tCSC 242 Intro To Programming"<<endl;
+	cout<<"Lab #: 			\tA1"<<endl;
+
+
+	return 0;
+}
+
---
+++ A1_Figueroa/A1_2/a1_2.cpp
@@ -0,0 +1,29 @@
+/*-------------------------------------------------------------------
+/	Author:		Michael Figueroa
+/	Date:		Feb 6, 2018
+/	Class:		CSC 242
+/	Professor:	Dr. Ahmad R. Hadaegh
+/	Assignment: A1_2
+---------------------------------------------------------------------
+*/
+
+#include <iostream>
+using namespace std;
+
+int main_A1_2(){
+
+	cout<<"Top 5 People in Erikson"<<endl;
+	cout<<"|***************************************************************|"<<endl;
+	cout<<"|\tLastName\tFirstName\tPosition\tSalary\t|"<<endl;
+	cout<<"|\t~~~~~~~~~\t~~~~~~~~~\t~~~~~~~~\t~~~~~~~~|"<<endl;
+	cout<<"|\tSmith    \tAdam     \tDirector\t99,010\t|"<<endl;
+	cout<<"|\tHadden   \tMary     \tDirector\t95,220\t|"<<endl;
+	cout<<"|\tSimpson  \tMike     \tManager \t80,000\t|"<<endl;
+	cout<<"|\tWilliams \tAdam     \tDesigner\t70,000\t|"<<endl;
+	cout<<"|\tAmer     \tAli      \tDesigner\t55,000\t|"<<endl;
+	cout<<"|***************************************************************|"<<endl;
+	cout<<"Have a good day"<<endl;
+	return 0;
+}
+
+
